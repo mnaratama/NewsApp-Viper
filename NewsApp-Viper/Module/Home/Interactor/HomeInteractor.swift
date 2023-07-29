e84aed077ff4ce0b1e86b41a4fef7e59d970f506
mnaratama <mnaratama@gmail.com>
2023-07-29 14:44:01 +0700

    Finish Home Entity, Interactor & Presenter

---
+++ NewsApp-Viper/Module/Home/Interactor/HomeInteractor.swift
@@ -0,0 +1,29 @@
+//
+//  HomeInteractor.swift
+//  NewsApp-Viper
+//
+//  Created by Naratama on 29/07/23.
+//
+
+import Foundation
+import Alamofire
+
+class HomeInteractor {
+    
+    weak var presenter: HomeInteractorOutput?
+    
+    private let service: NewsServiceProtocol = NewsService()
+}
+
+extension HomeInteractor: HomeUseCase {
+    
+    func getNews(param: [String : Any]) {
+        service.getHomeNews(param: param) { [weak self] response in
+            if let data = response?.articles {
+                self?.presenter?.successGetNews(data: data)
+            }
+        } error: { [weak self] err in
+            self?.presenter?.errorGetNews(message: err)
+        }
+    }
+}
---
+++ NewsApp-Viper/Module/Home/Presenter/HomePresenter.swift
@@ -0,0 +1,90 @@
+//
+//  HomePresenter.swift
+//  NewsApp-Viper
+//
+//  Created by Naratama on 29/07/23.
+//
+
+import UIKit
+
+class HomePresenter {
+
+    weak var view: HomeView?
+    var interactor: HomeUseCase?
+    var router: HomeWireFrame?
+    
+    var topHeadlineArray: [NewsModel] = [NewsModel]()
+    var refreshControl: UIRefreshControl = UIRefreshControl()
+    var category: String = CategoryType.general.rawValue
+    var categoryList: [CategoryType] = [
+        .business,
+        .entertainment,
+        .general,
+        .health,
+        .science,
+        .sports,
+        .technology
+    ]
+    var selectedCategory: CategoryType?
+    var searchText: String = ""
+    var pageSize: Int = 100
+    
+    private var model: HomeModel?
+}
+
+extension HomePresenter: HomePresentation {
+    func viewDidLoad() {
+        getNews()
+    }
+    
+    func getNews() {
+        var param: [String: Any] = [String: Any]()
+        param["category"] = category
+        param["pageSize"] = pageSize
+        view?.showLoading()
+        interactor?.getNews(param: param)
+    }
+    
+    func navigateToDetail(data: NewsModel) {
+        var param: [String: Any] = [String: Any]()
+        param["news"] = data
+        router?.navigateToDetail(data: param)
+    }
+    
+    func presentCategoryPicker() {
+        let categoryName = categoryList.map { $0.rawValue }
+        router?.presentCategoryPicker(categoryName: categoryName, categoryList: categoryList)
+    }
+    
+    func searchNews(searchText: String) {
+        let unfiltered = model?.news ?? []
+        
+        let filteredNews = unfiltered.filter { news in
+            return news.title?.lowercased().contains(searchText.lowercased()) ?? true
+        }
+        
+        if filteredNews.isEmpty && !searchText.isEmpty {
+            view?.showErrorSearch(isHidden: false)
+        } else {
+            view?.showErrorSearch(isHidden: true)
+        }
+        
+        self.topHeadlineArray = searchText.isEmpty ? unfiltered : filteredNews
+            
+        view?.reloadTableView()
+    }
+}
+
+extension HomePresenter: HomeInteractorOutput {
+    func successGetNews(data: [NewsModel]) {
+        view?.hideLoading()
+        topHeadlineArray.append(contentsOf: data)
+        model = HomeModel(news: data)
+        view?.reloadTableView()
+    }
+    
+    func errorGetNews(message: String) {
+        view?.hideLoading()
+        view?.showAlert(message: message)
+    }
+}
