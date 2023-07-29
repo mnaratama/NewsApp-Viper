635428ffc1abcea06c2b187922abe80eb9129735
mnaratama <mnaratama@gmail.com>
2023-07-29 15:16:35 +0700

    Finish Home Contract and Router

---
+++ NewsApp-Viper/Module/Home/Protocol/HomeContract.swift
@@ -0,0 +1,55 @@
+//
+//  HomeContract.swift
+//  NewsApp-Viper
+//
+//  Created by Naratama on 29/07/23.
+//
+
+import UIKit
+
+protocol HomeView: AnyObject {
+    var presenter: HomePresentation? { get set }
+    
+    func showErrorSearch(isHidden: Bool)
+    func showLoading()
+    func hideLoading()
+    func reloadTableView()
+    func showAlert(message: String)
+}
+
+protocol HomeUseCase: AnyObject {
+    var presenter: HomeInteractorOutput? { get set }
+    
+    func getNews(param: [String: Any])
+}
+
+protocol HomeWireFrame: AnyObject {
+    func createModule(data: [String: Any]?) -> BaseViewController
+    
+    func navigateToDetail(data: [String: Any])
+    func presentCategoryPicker(categoryName: [String], categoryList: [CategoryType])
+}
+
+protocol HomePresentation: AnyObject {
+    var view: HomeView? { get set }
+    var interactor: HomeUseCase? { get set }
+    var router: HomeWireFrame? { get set }
+    
+    var topHeadlineArray: [NewsModel] { get set }
+    var refreshControl: UIRefreshControl { get set }
+    var category: String { get set }
+    var categoryList: [CategoryType] { get set }
+    var selectedCategory: CategoryType? { get set }
+    var pageSize: Int { get set }
+    
+    func viewDidLoad()
+    func getNews()
+    func navigateToDetail(data: NewsModel)
+    func presentCategoryPicker()
+    func searchNews(searchText: String)
+}
+
+protocol HomeInteractorOutput: AnyObject {
+    func successGetNews(data: [NewsModel])
+    func errorGetNews(message: String)
+}
---
+++ NewsApp-Viper/Module/Home/Router/HomeRouter.swift
@@ -0,0 +1,57 @@
+//
+//  HomeRouter.swift
+//  NewsApp-Viper
+//
+//  Created by Naratama on 29/07/23.
+//
+
+import UIKit
+
+class HomeRouter: BaseRouter {
+    
+    weak var viewController: HomeViewController?
+    
+    override func createModule(data: [String : Any]? = nil) -> BaseViewController {
+        let view: BaseViewController & HomeView = HomeViewController()
+        let presenter: HomePresentation & HomeInteractorOutput = HomePresenter()
+        let interactor: HomeUseCase = HomeInteractor()
+        let router: HomeWireFrame = self
+        
+        view.presenter = presenter
+        presenter.view = view
+        presenter.router = router
+        presenter.interactor = interactor
+        interactor.presenter = presenter
+        viewController = view as? HomeViewController
+        return view
+    }
+}
+
+extension HomeRouter: HomeWireFrame {
+    func navigateToDetail(data: [String : Any]) {
+        
+    }
+    
+    func presentCategoryPicker(categoryName: [String], categoryList: [CategoryType]) {
+        
+    }
+    
+//    func navigateToDetail(data: [String : Any]) {
+//        guard let viewController = viewController else { return }
+//        let detailView = DetailNewsViewController()
+//        detailView.data = data
+//        detailView.modalPresentationStyle = .fullScreen
+//        viewController.present(detailView, animated: true)
+//    }
+    
+//    func presentCategoryPicker(categoryName: [String], categoryList: [CategoryType]) {
+//        guard let viewController = viewController else { return }
+//        let vc = CategoryPickerViewController()
+//        vc.delegate = viewController
+//        vc.pickerArray = categoryName
+//        vc.categoryArray = categoryList
+//        vc.modalPresentationStyle = .overFullScreen
+//        viewController.present(vc, animated: false, completion: nil)
+//    }
+}
+
